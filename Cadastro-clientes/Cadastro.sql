027fc27f594c03446bb431a3a6baa9813c390827
Jefferson-Araujo-Silva <jefferson.silva@sptech.school>
2022-09-01 16:18:54 -0300

    Atualizando a tabela Cadastro

--- Cadastro-clientes/Cadastro.sql
+++ Cadastro-clientes/Cadastro.sql
@@ -3,18 +3,20 @@ use sprint1;
 
 CREATE TABLE CadastroCliente (
 	idCliente INT PRIMARY KEY AUTO_INCREMENT, -- Identificação do cliente
-	tipoPessoa VARCHAR (08), CONSTRAINT chkPessoa CHECK(tipoPessoa = 'Jurídica'), -- Fisica ou juridica?
-    Nome VARCHAR (60), -- Nome completo ou Nome da empresa
+    NomeEmpresa VARCHAR (60), --  Nome da empresa
 	CNPJ CHAR (18), -- Deve conter (. / -)
 	Logradouro VARCHAR (60), -- Rua
     Numero INT,
 	Bairro VARCHAR (40), 
-    Complemento VARCHAR (40),
-    Cidade VARCHAR (20), 
-	CEP CHAR (08), 
+    Complemento VARCHAR (40) DEFAULT ('Não tem'),
+    Cidade VARCHAR (40), 
+	CEP CHAR (09), 
     idRastreio INT, -- Referência para localizar barril 
-    Marca VARCHAR (30),
-    tipoVinho VARCHAR (10) NOT NULL, CONSTRAINT chkVinho CHECK(tipoVinho IN('Tinto','Rosé','Brancos','Espumantes')),-- Cliente deve colocar o tipo de vinho que vai armazenar
-    Quantidade INT NOT NULL, -- Quantidade do produto
-    dataLimite DATE
+    tipoVinho VARCHAR (10) NOT NULL, CONSTRAINT chkVinho CHECK(tipoVinho IN('Tinto','Rosé','Branco')),-- Cliente deve colocar o tipo de vinho que vai armazenar
+    Quantidade INT NOT NULL -- Quantidade do produto em litros
 ); 
+
+desc CadastroCliente;
+
+drop table cadastrocliente;
+

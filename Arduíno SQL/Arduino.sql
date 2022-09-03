ae4562a8ac07a858c3a9c9f15b099dbe2f944d00
MResendeSilva <mateus.rsilva@sptech.school>
2022-09-03 13:16:47 -0300

    Tabelas Arduino e Cadastro foram atualizadas - adicionados os campos: Email_Login , Senha na tabela CadastroCliente. E foram criados os scripts de inserção e selects

--- Arduíno SQL/Arduino.sql
+++ Arduíno SQL/Arduino.sql
@@ -1,16 +1,25 @@
-use sprint1;
+USE sprint1;
 
-create table Arduino(
-ID int primary key auto_increment,
-Temperatura_Cº decimal(4,2),
-Umidade char(7),
-DataHora datetime
+CREATE TABLE Arduino(
+idCliente INT PRIMARY KEY AUTO_INCREMENT,
+NomeEmpresa VARCHAR (60),
+Temperatura_Cº DECIMAL(4,2),
+Umidade CHAR(7),
+DataHora DATETIME
 );
 
-insert into Arduino (Temperatura_Cº, Umidade, DataHora) values
-(23.3, '70%', '2022-08-23 15:31:02');
+INSERT INTO Arduino (Temperatura_Cº, Umidade, DataHora) VALUES
+(16.0, '70%', '2022-09-08 10:50:05'),
+(15.4, '67%', '2022-09-08 10:51:32'),
+(12.8, '71,2%', '2022-09-08 10:52:14');
 
-select * from Arduino;
 
-Drop table arduino;
+SELECT * FROM Arduino;
+SELECT * FROM Arduino WHERE idCliente = 1;
+SELECT * FROM Arduino WHERE idCliente = 2;
+SELECT * FROM Arduino WHERE idCliente = 3;
 
+
+SELECT Temperatura_C, Umidade, DataHora FROM Arduino WHERE idCliente = 1;
+SELECT Temperatura_C, Umidade, DataHora FROM Arduino WHERE idCliente = 2;
+SELECT Temperatura_C, Umidade, DataHora FROM Arduino WHERE idCliente = 3;
--- Cadastro-clientes/Cadastro.sql
+++ Cadastro-clientes/Cadastro.sql
@@ -1,22 +1,66 @@
-create database sprint1;
-use sprint1;
+CREATE DATABASE sprint1;
+USE sprint1;
 
 CREATE TABLE CadastroCliente (
 	idCliente INT PRIMARY KEY AUTO_INCREMENT, -- Identificação do cliente
-    NomeEmpresa VARCHAR (60), --  Nome da empresa
-	CNPJ CHAR (18), -- Deve conter (. / -)
+    NomeEmpresa VARCHAR (60) NOT NULL, --  Nome da empresa
+	CNPJ CHAR (18) NOT NULL, -- Deve conter (. / -) xx.xxx.xxx/xxxx-xx
+    Email_Login VARCHAR (80) NOT NULL,
+    Senha VARCHAR (30) NOT NULL,
 	Logradouro VARCHAR (60), -- Rua
     Numero INT,
 	Bairro VARCHAR (40), 
-    Complemento VARCHAR (40) DEFAULT ('Não tem'),
+    Complemento VARCHAR (40),
     Cidade VARCHAR (40), 
-	CEP CHAR (09), 
-    idRastreio INT, -- Referência para localizar barril 
+	CEP CHAR (09) NOT NULL, 
     tipoVinho VARCHAR (10) NOT NULL, CONSTRAINT chkVinho CHECK(tipoVinho IN('Tinto','Rosé','Branco')),-- Cliente deve colocar o tipo de vinho que vai armazenar
     Quantidade INT NOT NULL -- Quantidade do produto em litros
 ); 
 
-desc CadastroCliente;
 
-drop table cadastrocliente;
+INSERT INTO CadastroCliente (NomeEmpresa, CNPJ,Email_Login, Senha, Logradouro, Numero, Bairro, Complemento, Cidade, CEP, tipoVinho, Quantidade) VALUES
+('Vivivinhos', '20.645.889/0001-30','vivivinhos@createemail.com','altertable123','Rua Dados', 595, 'Database', 'Sala 11', 'São Paulo','09420-905','Tinto', 280),
+('Adegas Yoshi', '33.429.728/0002-40','adegayoshi@uia.tome','input#in_senha','Rua Algoritmos', 999, 'Tome', 'sala 05', 'São Paulo','02014-222','Rosé', 200),
+('Chola`s Cyber Winery', '11.000.101/1001-11','Winery@binary.tsmc','SenhaEmBinario','Rua Turing', 0101, 'Neuman', null , 'São Paulo','01111-000','Branco', 130);
+
+truncate CadastroCliente;
+
+
+SELECT*FROM CadastroCliente;
+SELECT*FROM CadastroCliente WHERE idCliente = 1;
+SELECT*FROM CadastroCliente WHERE tipoVinho =  'Tinto';
+
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 1;
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'V%';
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%o_';
+SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 1;
+
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =1;
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '_i%';
+
+
+
+SELECT*FROM CadastroCliente WHERE idCliente = 2;
+SELECT*FROM CadastroCliente WHERE tipoVinho =  'Rosé';
+
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 2;
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'A%';
+SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%h_';
+SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 2;
+
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =2;
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '% y%';
+
+
+
+SELECT*FROM CadastroCliente WHERE idCliente = 3;
+SELECT*FROM CadastroCliente WHERE tipoVinho =  'Branco';
+
+SELECT idCliente, NomeEmpresa, CNPJ, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 3;
+SELECT idCliente, NomeEmpresa, CNPJ, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'C%';
+SELECT idCliente, NomeEmpresa, CNPJ, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%r_';
+SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 3;
+
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =3;
+SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '%b%';
 

e4e209b0c31cdab83b6f4c2c20e5b6c71cb17075
MRESENDESILVA <mateus.rsilva@sptech.school>
2022-09-05 22:34:42 -0300

    Adição do campo "Celular_telefone", portanto, foram adicionados novos campos nos selects e nos inserts

--- Cadastro-clientes/Cadastro.sql
+++ Cadastro-clientes/Cadastro.sql
@@ -5,6 +5,7 @@ CREATE TABLE CadastroCliente (
 	idCliente INT PRIMARY KEY AUTO_INCREMENT, -- Identificação do cliente
     NomeEmpresa VARCHAR (60) NOT NULL, --  Nome da empresa
 	CNPJ CHAR (18) NOT NULL, -- Deve conter (. / -) xx.xxx.xxx/xxxx-xx
+	Celular_telefone VARCHAR (25), -- Deve conter (ddd) xxxxx-xxxx ou (ddd) xxxx-xxxx
     Email_Login VARCHAR (80) NOT NULL,
     Senha VARCHAR (30) NOT NULL,
 	Logradouro VARCHAR (60), -- Rua
@@ -18,54 +19,52 @@ CREATE TABLE CadastroCliente (
 ); 
 
 
-INSERT INTO CadastroCliente (NomeEmpresa, CNPJ,Email_Login, Senha, Logradouro, Numero, Bairro, Complemento, Cidade, CEP, tipoVinho, Quantidade) VALUES
-('Vivivinhos', '20.645.889/0001-30','vivivinhos@createemail.com','altertable123','Rua Dados', 595, 'Database', 'Sala 11', 'São Paulo','09420-905','Tinto', 280),
-('Adegas Yoshi', '33.429.728/0002-40','adegayoshi@uia.tome','input#in_senha','Rua Algoritmos', 999, 'Tome', 'sala 05', 'São Paulo','02014-222','Rosé', 200),
-('Chola`s Cyber Winery', '11.000.101/1001-11','Winery@binary.tsmc','SenhaEmBinario','Rua Turing', 0101, 'Neuman', null , 'São Paulo','01111-000','Branco', 130),
-('FazSentidoVinhos', '21.605.139/0001-50','danvinhos@makeanysense.com','papai123','Rua Rio de Janeiro', 123, 'FazAlgumsentido', 'Sala 08', 'Rio de Janeiro','03920-005','Rosé', 334),
-('VinhosDaCaramico', '25.501.328/0001-20', 'vinhoscaramico@stylee.com', 'love', '321','bilíngue', null , 'São Paulo', '08411-340', 'Branco', '320');
+INSERT INTO CadastroCliente (NomeEmpresa, CNPJ, Celular_telefone, Email_Login, Senha, Logradouro, Numero, Bairro, Complemento, Cidade, CEP, tipoVinho, Quantidade) VALUES
+('Vivivinhos', '20.645.889/0001-30', '(011) 2525-3030 ','vivivinhos@createemail.com','altertable123','Rua Dados', 595, 'Database', 'Sala 11', 'São Paulo','09420-905','Tinto', 280),
+('Adegas Yoshi', '33.429.728/0002-40', '(011) 98765-3131','adegayoshi@uia.tome','input#in_senha','Rua Algoritmos', 999, 'Tome', 'sala 05', 'São Paulo','02014-222','Rosé', 200),
+('Chola`s Cyber Winery', '11.000.101/1001-11','(011) 1111-1001','Winery@binary.tsmc','SenhaEmBinario','Rua Turing', 0101, 'Neuman', null , 'São Paulo','01111-000','Branco', 130),
+('FazSentidoVinhos', '21.605.139/0001-50', '(011) 98231-3999','danvinhos@makeanysense.com','papai123','Rua Rio de Janeiro', 123, 'FazAlgumsentido', 'Sala 08', 'Rio de Janeiro','03920-005','Rosé', 334),
+('VinhosDaCaramico', '25.501.328/0001-20', '(011) 95211-0093', 'vinhoscaramico@stylee.com', 'love', '321','bilíngue', null , 'São Paulo', '08411-340', 'Branco', '320');
 
-
-truncate CadastroCliente;
 
 
 SELECT*FROM CadastroCliente;
 SELECT*FROM CadastroCliente WHERE idCliente = 1;
 SELECT*FROM CadastroCliente WHERE tipoVinho =  'Tinto';
 
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 1;
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'V%';
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%o_';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 1;
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'V%';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%o_';
 SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 1;
 
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =1;
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '_i%';
+SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =1;
+SELECT idCliente, NomeEmpresa, Celular_telefone,Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '_i%';
 
 
 
 SELECT*FROM CadastroCliente WHERE idCliente = 2;
 SELECT*FROM CadastroCliente WHERE tipoVinho =  'Rosé';
 
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 2;
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'A%';
-SELECT idCliente, NomeEmpresa, CNPJ,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%h_';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 2;
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'A%';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone,Email_Login, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%h_';
 SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 2;
 
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =2;
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '% y%';
+SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =2;
+SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '% y%';
 
 
 
 SELECT*FROM CadastroCliente WHERE idCliente = 3;
 SELECT*FROM CadastroCliente WHERE tipoVinho =  'Branco';
 
-SELECT idCliente, NomeEmpresa, CNPJ, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 3;
-SELECT idCliente, NomeEmpresa, CNPJ, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'C%';
-SELECT idCliente, NomeEmpresa, CNPJ, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%r_';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone, CEP, tipoVinho, Quantidade FROM CadastroCliente WHERE idCliente = 3;
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE 'C%';
+SELECT idCliente, NomeEmpresa, CNPJ, Celular_telefone, CEP, tipoVinho, Quantidade FROM CadastroCliente  WHERE nomeEmpresa LIKE '%r_';
 SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 3;
 
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =3;
-SELECT idCliente, NomeEmpresa, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '%b';
+SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =3;
+SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '%b';
 
 
 

9487e02b374a8df0d559110b9cc0871bc19ec889
glvpedro <pedro.gsilva@sptech.school>
2022-09-07 14:31:09 -0300

    corrigindo espaçamento entre linhas na tabela de cadastro

--- Cadastro-clientes/Cadastro.sql
+++ Cadastro-clientes/Cadastro.sql
@@ -18,15 +18,12 @@ CREATE TABLE CadastroCliente (
     Quantidade INT NOT NULL -- Quantidade do produto em litros
 ); 
 
-
 INSERT INTO CadastroCliente (NomeEmpresa, CNPJ, Celular_telefone, Email_Login, Senha, Logradouro, Numero, Bairro, Complemento, Cidade, CEP, tipoVinho, Quantidade) VALUES
 ('Vivivinhos', '20.645.889/0001-30', '(011) 2525-3030 ','vivivinhos@createemail.com','altertable123','Rua Dados', 595, 'Database', 'Sala 11', 'São Paulo','09420-905','Tinto', 280),
 ('Adegas Yoshi', '33.429.728/0002-40', '(011) 98765-3131','adegayoshi@uia.tome','input#in_senha','Rua Algoritmos', 999, 'Tome', 'sala 05', 'São Paulo','02014-222','Rosé', 200),
 ('Chola`s Cyber Winery', '11.000.101/1001-11','(011) 1111-1001','Winery@binary.tsmc','SenhaEmBinario','Rua Turing', 0101, 'Neuman', null , 'São Paulo','01111-000','Branco', 130),
 ('FazSentidoVinhos', '21.605.139/0001-50', '(011) 98231-3999','danvinhos@makeanysense.com','papai123','Rua Rio de Janeiro', 123, 'FazAlgumsentido', 'Sala 08', 'Rio de Janeiro','03920-005','Rosé', 334),
 ('VinhosDaCaramico', '25.501.328/0001-20', '(011) 95211-0093', 'vinhoscaramico@stylee.com', 'love', 'Avenida Grungee','321','bilingue', null , 'São Paulo', '08411-340', 'Tinto', '320');
-
-
 
 SELECT*FROM CadastroCliente;
 SELECT*FROM CadastroCliente WHERE idCliente = 1;
@@ -40,8 +37,6 @@ SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 1;
 SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =1;
 SELECT idCliente, NomeEmpresa, Celular_telefone,Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '_i%';
 
-
-
 SELECT*FROM CadastroCliente WHERE idCliente = 2;
 SELECT*FROM CadastroCliente WHERE tipoVinho =  'Rosé';
 
@@ -52,8 +47,6 @@ SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 2;
 
 SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =2;
 SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '% y%';
-
-
 
 SELECT*FROM CadastroCliente WHERE idCliente = 3;
 SELECT*FROM CadastroCliente WHERE tipoVinho =  'Branco';
@@ -66,6 +59,4 @@ SELECT NomeEmpresa, Email_Login, Senha FROM CadastroCliente WHERE idCliente= 3;
 SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE idCliente =3;
 SELECT idCliente, NomeEmpresa, Celular_telefone, Logradouro, Numero, Bairro, Complemento, Cidade, CEP FROM CadastroCliente WHERE NomeEmpresa LIKE '%b';
 
-
 DROP TABLE CadastroCliente;
-
